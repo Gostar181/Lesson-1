a388a68f829ccceeb5916450778a5d8cc517f3b5
gosta <gostar181@gmail.com>
2024-12-21 11:21:02 +0200

    Дз 5.Бібліотека colorama працює з зміненням візуалу тексту,курсору та кольору

---
+++ 5 less.py
@@ -0,0 +1,57 @@
+import inspect
+import sys
+import colorama
+import requests
+'''
+def first_function():
+    pass
+
+class Human:
+    pass
+
+
+rq = requests
+first_f = first_function()
+nick = Human
+print(requests.__name__)
+print(rq.__name__)
+print(first_function.__name__)
+print(first_f.__ne__)
+print(Human.__name__)
+print(nick.__name__)
+print(__name__)
+
+
+intro_list = []
+
+for metod in dir(requests):
+    print(metod)
+
+
+print(inspect.ismodule(requests))
+print(inspect.isclass(requests))
+
+
+print(sys.executable)
+print(sys.version)
+print(sys.platform)
+print(sys.argv)
+
+for module_name, module_path in sys.modules.items():
+    print(module_name,module_path)
+
+'''
+for meto in dir(colorama):
+    print(meto)
+
+print(inspect.ismodule(colorama))
+print(inspect.isclass(colorama))
+
+
+
+
+
+
+
+
+
